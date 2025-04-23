37bd9764f83b19f7bcee6b02e9c81261c46ea5c2
yuxibo <yuxibo@tcl.com>
2025-04-23 15:36:48 +0800

    add I2C and MPU9250 initialization in MainTask.cpp

--- Core/Src/MainTask.cpp
+++ Core/Src/MainTask.cpp
@@ -7,9 +7,12 @@
 #include "cybergear.h"
 #include "UART.h"
 #include "MTI200.h"
+#include "I2C.h"
+#include "MPU9250.h"
 
 extern FDCAN_HandleTypeDef hfdcan1;
 extern UART_HandleTypeDef huart2;
+extern I2C_HandleTypeDef hi2c1;
 
 UART *uartLog = NULL;
 
@@ -20,31 +23,31 @@ extern "C" {
 #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 #else
 #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#endif 
+#endif
 PUTCHAR_PROTOTYPE
 {
-  if (uartLog) {
-    uartLog->Write((uint8_t *)&ch, 1);
-  } else {
-    HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
-  }
-  return ch;
+    if (uartLog) {
+        uartLog->Write((uint8_t *)&ch, 1);
+    } else {
+        HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    }
+    return ch;
 }
 
 // 串口输入重定向
 #ifdef __GNUC__
-int __io_getchar (void)
+int __io_getchar(void)
 #else
-int fgetc (FILE *f)
+int fgetc(FILE *f)
 #endif /* __GNUC__ */
 {
-  uint8_t ch = 0;
-  if (uartLog) {
-    ch = uartLog->ReadBlocking();
-  } else {
-    HAL_UART_Receive(&huart2, (uint8_t*)&ch, 1, HAL_MAX_DELAY);
-  }
-  return ch;
+    uint8_t ch = 0;
+    if (uartLog) {
+        ch = uartLog->ReadBlocking();
+    } else {
+        HAL_UART_Receive(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    }
+    return ch;
 }
 
 } // extern "C"
@@ -52,6 +55,31 @@ int fgetc (FILE *f)
 void MainTask(void const *pvParameters)
 {
     int ret = 0;
+
+    I2C *i2c = new I2C(&hi2c1, 0xD0);
+    if (i2c == NULL) {
+        TL_LOG_E("I2C object creation failed!");
+        return;
+    }
+
+    MPU9250 *mpu9250 = new MPU9250(i2c);
+    if (mpu9250 == NULL) {
+        TL_LOG_E("MPU9250 object creation failed!");
+    }
+
+    if (mpu9250) {
+        mpu9250->Configure(MPU9250::ACCEL_RANGE_4G, MPU9250::GYRO_RANGE_2000DPS);
+        // be lower than half the sample rate to avoid aliasing problems
+        // however best results are seen with 250 Hz gyro bandwidth, even though we sample at 200 Hz
+        mpu9250->setFilt(MPU9250::DLPF_BANDWIDTH_92HZ, MPU9250::DLPF_BANDWIDTH_250HZ);
+        // mpu9250->setFilt(MPU9250::DLPF_BANDWIDTH_OFF, MPU9250::DLPF_BANDWIDTH_OFF);
+        // mpu9250->setFilt(MPU9250::DLPF_BANDWIDTH_184HZ, MPU9250::DLPF_BANDWIDTH_184HZ);
+        // mpu9250->setFilt(MPU9250::DLPF_BANDWIDTH_41HZ, MPU9250::DLPF_BANDWIDTH_41HZ);
+        // mpu9250->setFilt(MPU9250::DLPF_BANDWIDTH_20HZ, MPU9250::DLPF_BANDWIDTH_20HZ);
+        // mpu9250->setFilt(MPU9250::DLPF_BANDWIDTH_OFF, MPU9250::DLPF_BANDWIDTH_184HZ);
+
+        mpu9250->ConfigureInterrupt(GPIOE, GPIO_PIN_3);
+    }
 
     // init uart for log
     uartLog = new UART(&huart2, 115200, UART::UART_OP_MODE_INTERRUPT);
@@ -75,14 +103,14 @@ void MainTask(void const *pvParameters)
         TL_LOG_E("motor1(0x%x) creation failed!", MOTOR_CAN_ID);
     }
 
-    Cybergear *motor2 = new Cybergear(fdcan, MOTOR_CAN_ID-1);
+    Cybergear *motor2 = new Cybergear(fdcan, MOTOR_CAN_ID - 1);
     if (motor1 == NULL) {
-        TL_LOG_E("motor2(0x%x) creation failed!", MOTOR_CAN_ID-1);
+        TL_LOG_E("motor2(0x%x) creation failed!", MOTOR_CAN_ID - 1);
     }
 
-    Cybergear *motor3 = new Cybergear(fdcan, MOTOR_CAN_ID-2);
+    Cybergear *motor3 = new Cybergear(fdcan, MOTOR_CAN_ID - 2);
     if (motor1 == NULL) {
-        TL_LOG_E("motor3(0x%x) creation failed!", MOTOR_CAN_ID-2);
+        TL_LOG_E("motor3(0x%x) creation failed!", MOTOR_CAN_ID - 2);
     }
 
     // init uart for xsens IMU (MTi630)
@@ -108,7 +136,7 @@ void MainTask(void const *pvParameters)
     if (motor2) {
         ret = motor2->Probe();
         if (ret != 0) {
-            TL_LOG_E("motor2(0x%x) probe failed!", MOTOR_CAN_ID-1);
+            TL_LOG_E("motor2(0x%x) probe failed!", MOTOR_CAN_ID - 1);
         } else {
             motor2->Enable();
             osDelay(1000);
@@ -119,12 +147,12 @@ void MainTask(void const *pvParameters)
     if (motor3) {
         ret = motor3->Probe();
         if (ret != 0) {
-            TL_LOG_E("motor3(0x%x) probe failed!", MOTOR_CAN_ID-2);
+            TL_LOG_E("motor3(0x%x) probe failed!", MOTOR_CAN_ID - 2);
         } else {
             motor3->Enable();
             osDelay(1000);
             motor3->Disable();
-        }  
+        }
     }
 
     return;
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -573,14 +573,20 @@ static void MX_GPIO_Init(void)
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
-  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin : PE3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -603,10 +609,6 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 5, 0);
-  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
